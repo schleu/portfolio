9ca46f8351cce762f56f1d31ae2bd825ab56f260
Danilo Schleu <daniloschleu@gmail.com>
2023-02-02 15:40:29 -0300

    feat: projects page

--- src/router.tsx
+++ src/router.tsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./components/DefaultLayout";
 import { AppRoutes } from "./constant/AppRoutes";
 import { Home } from "./pages/Home";
-import { Projects } from "./pages/Project";
+import { Projects } from "./pages/Projects";
+import { ProjectsDetail } from "./pages/ProjectsDetail";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path={AppRoutes.HOME} element={<Home />} />
-        <Route path={AppRoutes.PROJECT} element={<Projects />} />
+        <Route path={AppRoutes.PROJECTS} element={<Projects />} />
+        <Route path={AppRoutes.PROJECT} element={<ProjectsDetail />} />
       </Route>
     </Routes>
   );
